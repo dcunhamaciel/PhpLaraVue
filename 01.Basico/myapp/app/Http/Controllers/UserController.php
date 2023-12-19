439a71e296450b7e342b4f0fa11f54ffae10f930
dcunhamaciel <dcunhamaciel@gmail.com>
2023-12-18 22:11:23 -0300

    Relacionamento N -> N

--- 01.Basico/myapp/app/Http/Controllers/UserController.php
+++ 01.Basico/myapp/app/Http/Controllers/UserController.php
@@ -18,6 +18,12 @@ class UserController extends Controller
 
     public function show(User $user)
     {
+        $user->load('teams');
+        //$user->teams()->attach(1);
+        //$user->teams()->detach(2);
+        //$user->teams()->sync([1, 3]);
+        //$user->teams()->syncWithoutDetaching([2]);
+
         return view('user', [
             'user' => $user
         ]);
